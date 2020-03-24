0926fece430222e838e8b06c1e1e8565ea246da3
Makoto <makottchan1117@gmail.com>
2020-03-24 10:26:36 +0000

    kadai1

---
+++ practice.php
@@ -0,0 +1,30 @@
+<php?
+echo 'hello php!';
+$a = 3;
+$b = 7;
+echo '$a+$b';
+$array_month = [1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月];
+echo $array_mouth[9];
+$Hello = Hello;
+$name = Makoto;
+$World = 'sWorld;
+echo $Hello $name $World;
+$tech_boost=" tech";
+$tech_boost=" boost";
+echo $tech_boost;
+
+$Calender_2018=[
+"January"=>"1月",
+"Feburary"=>"2月",
+"March"="3月",
+"April"=>"4月",
+"May"=>"5月",
+"June"=>"6月",
+"July"=>"7月",
+"Augast"=>"8月",
+"September"=>"9月",
+"Octorber"=>"10月",
+"November"=>"11月",
+"December"=>"12月",
+];
+echo Calender_2018[December];
