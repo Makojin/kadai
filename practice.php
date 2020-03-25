1d51c649969ef0e91fee3a7a04cbe1e022ddb9c9
Makoto <makottchan1117@gmail.com>
2020-03-25 08:43:26 +0000

    kadai1-1

--- practice.php
+++ practice.php
@@ -1,22 +1,22 @@
-<php?
+<?php
 echo 'hello php!';
 $a = 3;
 $b = 7;
-echo '$a+$b';
-$array_month = [1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月];
-echo $array_mouth[9];
-$Hello = Hello;
-$name = Makoto;
-$World = 'sWorld;
-echo $Hello $name $World;
-$tech_boost=" tech";
-$tech_boost=" boost";
+echo $a + $b;
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_mouth["7"];
+$Hello = "Hello";
+$name = "Makoto";
+$World = "'sWorld";
+echo $Hello. $name.$World;
+$tech_boost="tech";
+$tech_boost="boost";
 echo $tech_boost;
 
 $Calender_2018=[
 "January"=>"1月",
 "Feburary"=>"2月",
-"March"="3月",
+"March"=>"3月",
 "April"=>"4月",
 "May"=>"5月",
 "June"=>"6月",
@@ -27,4 +27,4 @@ $Calender_2018=[
 "November"=>"11月",
 "December"=>"12月",
 ];
-echo Calender_2018[December];
+echo $Calender_2018["December"];
